2555d18c916feab5d4a4704da0d6d560f61b401e
bad-plus <valeratracktor@gmail.com>
2025-12-06 15:00:27 +0100

    refactor

---
+++ src/world/generation/terrain_generator.cpp
@@ -0,0 +1,29 @@
+#include "world_generator.h"
+#include "../block.h"
+#include "../chunk.h"
+#include "biome.hpp"
+#include "../../core/logger.h"
+
+void WorldGenerator::generateTerrain(Chunk* chunk, int x, int z) {
+    for (int px = 0; px < CHUNK_SIZE_X; px++) {
+        for (int pz = 0; pz < CHUNK_SIZE_Z; pz++) {
+            int world_pos_x = (x * CHUNK_SIZE_X) + px;
+            int world_pos_z = (z * CHUNK_SIZE_Z) + pz;
+
+            auto weights = Biome::getBiomeWeight(m_perlin, world_pos_x, world_pos_z);
+            Biome::BiomeID biome = Biome::getDominantBiome(weights);
+            int height = Biome::getHeight(m_perlin, world_pos_x, world_pos_z, weights);
+
+            for (int py = 0; py < height; py++) {
+                BlockID block = BlockID::EMPTY;
+
+                if (py == height - 1) block = Biome::BiomesInfo[biome].top_block;
+                else if (py < height - 1 && py > height - 6) block = Biome::BiomesInfo[biome].under_block;
+                else if (py == 0) block = BlockID::BEDROCK;
+                else block = BlockID::STONE;
+
+                chunk->setBlock({ px, py, pz }, block, false);
+            }
+        }
+    }
+}
--- src/world/generation/world_generator.h
+++ src/world/generation/world_generator.h
@@ -10,6 +10,7 @@ public:
 
     Chunk* generateChunk(Chunk* chunk, int x, int z);
 private:
+    void generateTerrain(Chunk* chunk, int x, int z);
 
     int m_seed;
     Perlin2D* m_perlin;
