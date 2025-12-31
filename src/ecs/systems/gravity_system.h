9077c7674042cabedda12ae1ffba30b7510c8b92
bad-plus <valeratracktor@gmail.com>
2025-12-31 17:05:37 +0100

    refactor gravity

--- src/ecs/systems/gravity_system.h
+++ src/ecs/systems/gravity_system.h
@@ -4,10 +4,12 @@
 #include "../components/transform.h"
 #include "../components/velocity.h"
 #include "../components/player_mode.h"
+#include "../components/mass.h"
 #include <glm/glm.hpp>
 
 constexpr float GRAVITY = 20.0f;
 constexpr float TERMINAL_VELOCITY = -50.0f;
+constexpr float MASS_MULTIPLAYER = 1.0f;
 
 class GravitySystem {
 public:
@@ -23,9 +25,15 @@ public:
 			}
 
 			auto& vel = ecs.storage<Velocity>().get(e);
-			
+
+			float mass = 1.0f;
+
+			if (ecs.storage<Mass>().has(e)) {
+				mass = ecs.storage<Mass>().get(e).value;
+			}
+
 			if (vel.y > TERMINAL_VELOCITY) {
-				vel.y -= GRAVITY * delta_time;
+				vel.y -= (MASS_MULTIPLAYER * mass) * GRAVITY * delta_time;
 			}
 		}
 	}
